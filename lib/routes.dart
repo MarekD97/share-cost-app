565d6467e6d18bc4c0e4fe541669b3addfed1423
MarekD97 <mdorosz2@wp.pl>
2023-02-13 18:07:25 +0100

    Add views and update app routes

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,12 +1,22 @@
+import 'package:share_cost_app/views/create_expense_view.dart';
+import 'package:share_cost_app/views/dashboard_view.dart';
+import 'package:share_cost_app/views/home_view.dart';
 import 'package:share_cost_app/views/login_view.dart';
+import 'package:share_cost_app/views/signup_view.dart';
 
 class Routes {
 
   static const String home = '/';
+  static const String login = '/login';
+  static const String signup = '/signup';
   static const String dashboard = '/dashboard';
   static const String createExpense = '/create-expense';
 
   static final routes = {
-    home: (context) => const LoginView(),
+    home: (context) => const HomeView(),
+    login: (context) => const LoginView(),
+    signup: (context) => const SignupView(),
+    dashboard: (context) => const DashboardView(),
+    createExpense: (context) => const CreateExpenseView(),
   };
 }
---
+++ lib/views/create_expense_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CreateExpenseView extends StatefulWidget {
+  const CreateExpenseView({Key? key}) : super(key: key);
+
+  @override
+  State<CreateExpenseView> createState() => _CreateExpenseViewState();
+}
+
+class _CreateExpenseViewState extends State<CreateExpenseView> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
