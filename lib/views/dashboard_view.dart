dee5f0081dbb2317a6f3fe4af7d4581a11b23e21
MarekD97 <mdorosz2@wp.pl>
2023-02-14 12:51:20 +0100

    Add list view in the expense tab

--- lib/views/dashboard_view.dart
+++ lib/views/dashboard_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:share_cost_app/components/expense_card.dart';
 
 class DashboardView extends StatefulWidget {
   const DashboardView({Key? key}) : super(key: key);
@@ -8,22 +9,37 @@ class DashboardView extends StatefulWidget {
 }
 
 class _DashboardViewState extends State<DashboardView> {
+
+  final Widget _expenseTab = ListView.builder(
+    itemBuilder: (context, index) => ExpenseCard(
+        title: 'Item ${index + 1}',
+        price: (index * 10) % 13,
+        date: DateTime.now()),
+    itemCount: 20,
+    padding: const EdgeInsets.fromLTRB(10.0, 10.0, 10.0, 60.0),
+  );
+  final Widget _balanceTab = const Text('Balance Tab');
+
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
         length: 2,
         child: Scaffold(
-            appBar: AppBar(
-              title: const Text('Dashboard'),
-              bottom: const TabBar(
-                tabs: [Tab(text: 'Expenses'), Tab(text: 'Balance')],
-              ),
+          appBar: AppBar(
+            title: const Text('Dashboard'),
+            bottom: const TabBar(
+              tabs: [Tab(text: 'Expenses'), Tab(text: 'Balance')],
             ),
-            body: const TabBarView(
-              children: [
-                Text('Expenses Tab'),
-                Text('Balance Tab'),
-              ],
-            )));
+          ),
+          body: TabBarView(
+            children: [
+              _expenseTab,
+              _balanceTab,
+            ],
+          ),
+          floatingActionButton: FloatingActionButton(
+              onPressed: () {}, child: const Icon(Icons.add)),
+          ),
+        );
   }
 }
