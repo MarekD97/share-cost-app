106a5d124789df4936dbedd42346ec7f39feaf46
MarekD97 <mdorosz2@wp.pl>
2023-02-15 20:11:03 +0100

    Add create expense view

--- lib/views/create_expense_view.dart
+++ lib/views/create_expense_view.dart
@@ -1,4 +1,7 @@
+import 'package:currency_text_input_formatter/currency_text_input_formatter.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:intl/intl.dart';
 
 class CreateExpenseView extends StatefulWidget {
   const CreateExpenseView({Key? key}) : super(key: key);
@@ -8,8 +11,73 @@ class CreateExpenseView extends StatefulWidget {
 }
 
 class _CreateExpenseViewState extends State<CreateExpenseView> {
+  DateTime _selectedDate = DateTime.now();
+
+  final _dateController = TextEditingController();
+
+  Future _selectDate(context) async {
+    DateTime? selectedDate = await showDatePicker(
+        context: context,
+        initialDate: DateTime.now(),
+        firstDate: DateTime(2000),
+        lastDate: DateTime(2100));
+
+    if (selectedDate != null) {
+      _selectedDate = selectedDate;
+      _dateController.text = DateFormat.yMMMd().format(_selectedDate);
+      _dateController.selection = TextSelection.fromPosition(TextPosition(
+          offset: _dateController.text.length,
+          affinity: TextAffinity.upstream));
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    void onSubmitForm() {
+      //TODO: create an expense
+      Navigator.pop(context);
+    }
+
+    return Scaffold(
+        appBar: AppBar(title: const Text('Create new expense')),
+        body: Form(
+          child: ListView(
+            padding: const EdgeInsets.all(20.0),
+            shrinkWrap: true,
+            children: [
+              const TextField(
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(), labelText: 'Title'),
+              ),
+              const SizedBox(height: 20.0),
+              TextField(
+                keyboardType: TextInputType.number,
+                inputFormatters: <TextInputFormatter>[
+                  CurrencyTextInputFormatter(locale: 'pl', decimalDigits: 2)
+                ],
+                decoration: const InputDecoration(
+                    border: OutlineInputBorder(), labelText: 'Amount'),
+              ),
+              const SizedBox(height: 20.0),
+              TextField(
+                controller: _dateController,
+                keyboardType: TextInputType.none,
+                onTap: () {
+                  _selectDate(context);
+                },
+                decoration: const InputDecoration(
+                    border: OutlineInputBorder(), labelText: 'Date'),
+              ),
+              const SizedBox(height: 20.0),
+              const TextField(
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(), labelText: 'Paid by'),
+              ),
+              const SizedBox(height: 20.0),
+              ElevatedButton(
+                  onPressed: onSubmitForm, child: const Text('Create expense'))
+            ],
+          ),
+        ));
   }
 }
--- lib/views/dashboard_view.dart
+++ lib/views/dashboard_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:share_cost_app/components/expense_card.dart';
+import 'package:share_cost_app/routes.dart';
 
 class DashboardView extends StatefulWidget {
   const DashboardView({Key? key}) : super(key: key);
@@ -9,7 +10,6 @@ class DashboardView extends StatefulWidget {
 }
 
 class _DashboardViewState extends State<DashboardView> {
-
   final Widget _expenseTab = ListView.builder(
     itemBuilder: (context, index) => ExpenseCard(
         title: 'Item ${index + 1}',
@@ -18,28 +18,54 @@ class _DashboardViewState extends State<DashboardView> {
     itemCount: 20,
     padding: const EdgeInsets.fromLTRB(10.0, 10.0, 10.0, 60.0),
   );
-  final Widget _balanceTab = const Text('Balance Tab');
+  final Widget _balanceTab = Column(
+    children: [
+      SizedBox(
+        width: double.infinity,
+        height: 400.0,
+        child: ListView.builder(
+          itemBuilder: (context, index) => Row(
+            children: [
+              Text('Title $index'),
+              Container(
+                height: 20.0,
+                width: 200.0,
+                margin: EdgeInsets.all(4.000),
+              )
+            ],
+          ),
+          itemCount: 3,
+        ),
+      )
+    ],
+  );
 
   @override
   Widget build(BuildContext context) {
+    void navigateToCreateExpenseView() {
+      Navigator.pushNamed(context, Routes.createExpense);
+    }
+
     return DefaultTabController(
-        length: 2,
-        child: Scaffold(
-          appBar: AppBar(
-            title: const Text('Dashboard'),
-            bottom: const TabBar(
-              tabs: [Tab(text: 'Expenses'), Tab(text: 'Balance')],
-            ),
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Dashboard'),
+          bottom: const TabBar(
+            tabs: [Tab(text: 'Expenses'), Tab(text: 'Balance')],
           ),
-          body: TabBarView(
-            children: [
-              _expenseTab,
-              _balanceTab,
-            ],
-          ),
-          floatingActionButton: FloatingActionButton(
-              onPressed: () {}, child: const Icon(Icons.add)),
-          ),
-        );
+        ),
+        body: TabBarView(
+          children: [
+            _expenseTab,
+            _balanceTab,
+          ],
+        ),
+        floatingActionButton: FloatingActionButton(
+            onPressed: navigateToCreateExpenseView,
+            tooltip: 'Add an expense',
+            child: const Icon(Icons.add)),
+      ),
+    );
   }
 }
