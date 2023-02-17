1ae263dc966934e729a1c9f379b03670c4c2828e
MarekD97 <mdorosz2@wp.pl>
2023-02-17 23:17:17 +0100

    Add Cloud Firestore and implement addDoc and removeDocs functions for tests

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
 import 'package:share_cost_app/routes.dart';
@@ -12,10 +13,32 @@ class HomeView extends StatefulWidget {
 class _HomeViewState extends State<HomeView> {
   @override
   void initState() {
-    SchedulerBinding.instance.addPostFrameCallback((timeStamp) {
-      Navigator.pushReplacementNamed(context, Routes.login);
-    });
+    // SchedulerBinding.instance.addPostFrameCallback((timeStamp) {
+    //   Navigator.pushReplacementNamed(context, Routes.login);
+    // });
     super.initState();
+  }
+
+  void _addDoc() {
+    final db = FirebaseFirestore.instance;
+    db.collection('/users').add({
+      'name': 'Tom',
+      'age': 27,
+      'createdAt': Timestamp.now()
+    }).whenComplete(() => print('Success'));
+  }
+
+  void _removeDocs() {
+    final db = FirebaseFirestore.instance;
+    db.collection('/users').get().then((event) {
+      for (var doc in event.docs) {
+        doc.reference.delete();
+      }
+    }).then((event) {
+      db
+          .collection('/users')
+          .add({'name': 'Mark', 'age': 25, 'createdAt': Timestamp.now()});
+    });
   }
 
   @override
@@ -23,8 +46,20 @@ class _HomeViewState extends State<HomeView> {
     return Scaffold(
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
-        children: const [
-          Center(child: Text('Share Cost App')),
+        children: [
+          const Center(child: Text('Share Cost App')),
+          const SizedBox(height: 20.0,),
+          const Text('For debug purposes'),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              ElevatedButton(
+                  onPressed: _addDoc,
+                  child: const Text('Add the doc to Firebase')),
+              ElevatedButton(
+                  onPressed: _removeDocs, child: const Text('Remove all docs')),
+            ],
+          )
         ],
       ),
     );
