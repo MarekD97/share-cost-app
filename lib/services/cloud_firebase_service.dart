9e855ccba566fe143a3712738b69ab7479e44f17
MarekD97 <mdorosz2@wp.pl>
2023-02-20 23:17:01 +0100

    Update cloud firebase service: add getExpenseList function

--- lib/services/cloud_firebase_service.dart
+++ lib/services/cloud_firebase_service.dart
@@ -4,10 +4,18 @@ import 'package:share_cost_app/models/expense_list_model.dart';
 class CloudFirebaseService {
 
   static FirebaseFirestore _getInstance() => FirebaseFirestore.instance;
+  static const String expensesCollectionPath = '/expenses';
 
   static Future<String> createExpenseList(ExpenseList expenseList) async {
     final db = _getInstance();
-    final result = await db.collection('/expenses').add(expenseList.toJson());
+    final result = await db.collection(expensesCollectionPath).add(expenseList.toJson());
     return result.id;
   }
+
+  static Future<DocumentSnapshot> getExpenseList(String id) async {
+    final db = _getInstance();
+    final result = await db.collection(expensesCollectionPath).doc(id).get();
+    return result;
+  }
+
 }
