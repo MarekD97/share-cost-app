a83d091d30caa6694e520a9754e8cbb6cf7d6dea
MarekD97 <mdorosz2@wp.pl>
2023-02-21 14:10:39 +0100

    Add uuid to models

--- lib/models/expense_list_model.dart
+++ lib/models/expense_list_model.dart
@@ -1,15 +1,24 @@
 import 'package:share_cost_app/models/expense_model.dart';
 import 'package:share_cost_app/models/person_model.dart';
+import 'package:uuid/uuid.dart';
+
+const uuid = Uuid();
 
 class ExpenseList {
+  String id;
   List<Expense> expenses;
   List<Person> members;
   DateTime createdAt;
 
   ExpenseList(
-      {required this.expenses, required this.members, required this.createdAt});
+      {String? id,
+      required this.expenses,
+      required this.members,
+      required this.createdAt})
+      : id = id ?? uuid.v4();
 
   static ExpenseList fromJson(Map<String, dynamic> json) => ExpenseList(
+      id: json['id'],
       expenses: List.from(
           json['expenses'].map((expense) => Expense.fromJson(expense))),
       members:
@@ -17,6 +26,7 @@ class ExpenseList {
       createdAt: DateTime.parse(json['createdAt']));
 
   Map<String, dynamic> toJson() => <String, dynamic>{
+        'id': id,
         'expenses': expenses.map((expense) => expense.toJson()).toList(),
         'members': members.map((member) => member.toJson()).toList(),
         'createdAt': createdAt.toString()
--- lib/models/person_model.dart
+++ lib/models/person_model.dart
@@ -1,9 +1,15 @@
+import 'package:uuid/uuid.dart';
+
+const uuid = Uuid();
+
 class Person {
+  String id;
   String name;
 
-  Person({required this.name});
+  Person({String? id, required this.name}) : id = id ?? uuid.v4();
 
-  static Person fromJson(Map<String, dynamic> json) => Person(name: json['name']);
+  static Person fromJson(Map<String, dynamic> json) =>
+      Person(id: json['id'], name: json['name']);
 
-  Map<String, dynamic> toJson() => <String, dynamic>{'name': name};
+  Map<String, dynamic> toJson() => <String, dynamic>{'id': id, 'name': name};
 }
