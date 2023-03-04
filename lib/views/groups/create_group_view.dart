f2a64d93a936b612b3f0098e2322f4d80924ec46
MarekD97 <mdorosz2@wp.pl>
2023-03-04 23:26:45 +0100

    Update create group form

--- lib/views/groups/create_group_view.dart
+++ lib/views/groups/create_group_view.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:share_cost_app/models/group_model.dart';
 import 'package:share_cost_app/models/person_model.dart';
+import 'package:share_cost_app/services/cloud_firebase_service.dart';
 
 class CreateGroupView extends StatefulWidget {
   const CreateGroupView({Key? key}) : super(key: key);
@@ -9,7 +11,27 @@ class CreateGroupView extends StatefulWidget {
 }
 
 class _CreateGroupViewState extends State<CreateGroupView> {
-  List<Person> members = List.empty();
+  List<Person> members = List.empty(growable: true);
+  final _nameController = TextEditingController();
+  final _memberController = TextEditingController();
+
+  void _addMemberToList() => setState(() {
+        members.add(Person(name: _memberController.text));
+      });
+
+  void _removeMemberFromList(Person member) => setState(() {
+        members.remove(member);
+      });
+
+  void _submitForm() {
+    Group group = Group(
+        name: _nameController.text,
+        expenses: List.empty(),
+        members: members,
+        createdAt: DateTime.now());
+    CloudFirebaseService.createGroup(group);
+    Navigator.pop(context);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -19,18 +41,51 @@ class _CreateGroupViewState extends State<CreateGroupView> {
         shrinkWrap: true,
         padding: const EdgeInsets.all(8.0),
         children: [
-          const TextField(
-            decoration: InputDecoration(
-                border: OutlineInputBorder(), labelText: 'Name'),
-          ),
           TextField(
-            decoration: InputDecoration(
-                suffix: IconButton(
-                    onPressed: () {}, icon: const Icon(Icons.add)),
-                border: const OutlineInputBorder(),
-                labelText: 'Member'),
+            decoration: const InputDecoration(
+                border: OutlineInputBorder(), labelText: 'Name'),
+            controller: _nameController,
           ),
-          ElevatedButton(onPressed: () {}, child: const Text('Submit'))
+          Container(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text(
+                      'Member list:',
+                      style: TextStyle(fontSize: 16.0),
+                    ),
+                    ...members
+                        .map((member) => Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Text(member.name),
+                                  IconButton(
+                                    icon: const Icon(Icons.close),
+                                    onPressed: () =>
+                                        _removeMemberFromList(member),
+                                  )
+                                ]))
+                        .toList()
+                  ])),
+          Row(
+            children: [
+              Flexible(
+                child: TextField(
+                  decoration: const InputDecoration(
+                      border: OutlineInputBorder(), labelText: 'Member name'),
+                  controller: _memberController,
+                ),
+              ),
+              const SizedBox(width: 8.0),
+              ElevatedButton(
+                onPressed: _addMemberToList,
+                child: const Text('Add member'),
+              )
+            ],
+          ),
+          ElevatedButton(onPressed: _submitForm, child: const Text('Submit'))
         ],
       ),
     );
